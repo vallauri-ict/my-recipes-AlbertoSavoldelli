17244c75376c601220ec846daf219101466f0420
AlbertoSavoldelli <a.savoldelli.1375@vallauri.edu>
2021-12-16 10:22:46 +0100

    Json server and service created

---
+++ src/app/recipes.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { RecipeModel } from './models/recipes.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  constructor() { }
+}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { RecipeModel } from 'src/app/models/recipes.model';
+import { RecipesService } from 'src/app/recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,32 +9,10 @@ import { RecipeModel } from 'src/app/models/recipes.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeSelectedInList = new EventEmitter<RecipeModel>()
-  recipes:RecipeModel[]=[
-    new RecipeModel(
-      "Spaghetti alla chitarra",
-      "Un particolare tipo di pasta che...",
-      "https://images.lacucinaitaliana.it/wp-content/uploads/2017/02/Maccheroni-alla-chitarra.jpg"
-    ),
-    new RecipeModel(
-      "Lasagne alla bolognese",
-      "Pasta emiliana molto calorica sempre presente...",
-      "https://www.foodblog.it/wp-content/uploads/2020/01/lasagne.jpg"
-    ),
-    new RecipeModel(
-      "Gnocchi ai formaggi",
-      "Pasta piemontese",
-      "https://blog.giallozafferano.it/ricettiamoconmary/wp-content/uploads/2020/02/IMG_7710-960x625.jpg"
-    ),
-    new RecipeModel(
-      "Tiramisu",
-      "Classico dolce italiano",
-      "https://www.buttalapasta.it/wp-content/uploads/2012/04/ricetta-tiramisu-classico.jpg"
-    )
-
-  ]
-
+  recipes:RecipeModel[]=[];
   selectedRecipe:RecipeModel;
-  constructor() {
+  constructor(private recipesService:RecipesService) {
+    this.recipes=recipesService.recipes;
     this.selectedRecipe=this.recipes[0];
    }
 
