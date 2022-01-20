69e9c5ed7643456dfdcec5afcbc02a8a7a5f1f37
AlbertoSavoldelli <a.savoldelli.1375@vallauri.edu>
2022-01-20 11:26:58 +0100

    Go to shopping-list to revise

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,14 +7,10 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   
-  @Output() featureSelected = new EventEmitter<string>();
   collapsed:boolean=true;
   constructor() { }
 
   ngOnInit(): void {
     
-  }
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
   }
 }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RecipeService } from 'src/app/shared/recipe.service';
+import { ShoppingListService } from 'src/app/shared/shopping-list.service';
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor(public recipeService:RecipeService) { }
+
+  constructor(public recipeService:RecipeService, private shoppingList:ShoppingListService) { }
 
   ngOnInit(): void { }
-  // sendToShoppingList():void{
-  //   this.shoppingList
-  // }
+
+  sendToShoppingList=():void=>{
+    this.shoppingList.addIngredients(this.recipeService.selectedRecipe.ingredients);
+  }
 }
